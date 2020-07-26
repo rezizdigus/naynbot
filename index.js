b2495709971371e02e7eaafd76dd20424556ad2c
rezizdigus <rezizdiguspl@gmail.com>
2020-07-26 03:52:30 +0200

    Command Handler fix with reload command
    Thanks to discordjs.guide ;P

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
+const fs = require("fs");
 
 const config = require("./config.json");
+
+//command handler
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 activityState = 0;
 
@@ -44,5 +55,24 @@ client.on('ready', () => {
 	activityState = 1;
 	activityLoop(15000);
 });
+
+client.on('message', message => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get(commandName);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
 
 client.login(config.token);
